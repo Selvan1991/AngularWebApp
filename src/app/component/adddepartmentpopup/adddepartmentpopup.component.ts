5ad09ffa6b48abe81a792a35b570f7b2357bf7f1
Selvan1991 <128295341+Selvan1991@users.noreply.github.com>
2023-05-31 16:16:09 +0530

    Update adddepartmentpopup.component.ts
    removed the commented codes and also added alert popup message

--- src/app/component/adddepartmentpopup/adddepartmentpopup.component.ts
+++ src/app/component/adddepartmentpopup/adddepartmentpopup.component.ts
@@ -13,60 +13,48 @@ export class AdddepartmentpopupComponent implements OnInit {
   inputdata: any;
   editdata: any;
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private ref: MatDialogRef<AdddepartmentpopupComponent>, 
-  private buildr: FormBuilder,private service: StudentService) {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private ref: MatDialogRef<AdddepartmentpopupComponent>,
+    private buildr: FormBuilder, private service: StudentService) {
 
-}
-ngOnInit(): void {
-   this.inputdata = this.data;
-   
-  if(this.inputdata.code>0){
-    this.setpopupdata(this.inputdata.code)
   }
-}
+  ngOnInit(): void {
+    this.inputdata = this.data;
 
-async setpopupdata(code: any) {
-  this.service.GetDepartmentById(code).subscribe(item => {
-    this.editdata = item;
-    
-    this.departmentform.setValue({departmentname:this.editdata.departmentName, departmentid : this.editdata.departmentID})
+    if (this.inputdata.code > 0) {
+      this.setpopupdata(this.inputdata.code)
+    }
+  }
+
+  async setpopupdata(code: any) {
+    this.service.GetDepartmentById(code).subscribe(item => {
+      this.editdata = item;
+
+      this.departmentform.setValue({ departmentname: this.editdata.departmentName, departmentid: this.editdata.departmentID })
+    });
+  }
+  departmentform = this.buildr.group({
+    departmentname: this.buildr.control(''),
+    departmentid: this.buildr.control('')
   });
-}
-departmentform = this.buildr.group({
-  departmentname: this.buildr.control(''),
-  departmentid: this.buildr.control('')
-});
-// departmentform = this.buildr.group({
-//   departmentname: ['', Validators.required],
-//   password: ['', [Validators.required,Validators.minLength(6)]],
-//   confirmpassword: ['', Validators.required],
-//   UserAddress: this.fb.group({
-//   city: ['', Validators.required],
-//   state: ['', Validators.required],    
-//   pincode: ['', [Validators.required,Validators.pattern('^[0-9]$')]],    
-// }),
 
-// });
-async closepopup() {
-  this.ref.close('Closed using function');
-}
-async Savedepartment() {
-  this.departmentform.value;
-  if(this.departmentform.value.departmentid ==='')
-  {
-    this.departmentform.value.departmentid = '0';
+  async closepopup() {
+    this.ref.close('Closed using function');
   }
+  async Savedepartment() {
+    this.departmentform.value;
+    if (this.departmentform.value.departmentid === '') {
+      this.departmentform.value.departmentid = '0';
+    }
 
-  this.service.savedepartment(this.departmentform.value).subscribe(res => {
-    if(this.departmentform.value.departmentid ==='')
-  {
-    alert('Department has been Added Successfully');
+    this.service.savedepartment(this.departmentform.value).subscribe(res => {
+      if (this.inputdata.btntext !== 'Update') {
+        alert('Department has been Added Successfully');
+      }
+      else {
+        alert('Department has been updated Successfully');
+      }
+
+      this.closepopup();
+    });
   }
-  else{
-    alert('Department has been updated Successfully');
-  }
-    
-    this.closepopup();
-  });
- }
 }
